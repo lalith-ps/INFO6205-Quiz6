6f06c68c3a502c21588c0fe67acef22d25dd9a13
Sai Lalith Pulluri <pulluri.sa@northeastern.edu>
2025-03-27 13:08:00 -0400

    Quiz 06

--- src/main/java/QuickSort.java
+++ src/main/java/QuickSort.java
@@ -20,18 +20,30 @@ public class QuickSort {
     // quicksort the subarray from a[lo] to a[hi]
     private static void sort(Comparable[] a, int lo, int hi) {
         if (hi <= lo) return;
-       //Student TODO
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
     }
 
     // partition the subarray a[lo..hi] so that a[lo..j-1] <= a[j] <= a[j+1..hi]
     // and return the index j.
     private static int partition(Comparable[] a, int lo, int hi) {
-        //Student TODO
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+            // find item on lo to swap
+            while (less(a[++i], v)) if (i == hi) break;
+            // find item on hi to swap
+            while (less(v, a[--j])) if (j == lo) break;
+            // check if pointers cross
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        // put partitioning item v at a[j]
+        exch(a, lo, j);
+        return j;
     }
-
-    
-
-
 
    /***************************************************************************
     *  Helper sorting functions.
@@ -64,7 +76,6 @@ public class QuickSort {
         return true;
     }
 
-
     // print array to standard output
     private static void show(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
@@ -87,5 +98,4 @@ public class QuickSort {
         show(a);
         assert isSorted(a);
     }
-
 }
